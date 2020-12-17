d216cbdf67054d28ff457f593d109a5dcb67f0fd
ozoli321 <71514798+ozoli321@users.noreply.github.com>
2020-12-17 20:29:51 +0100

    Kész

---
+++ TiliToli/TiliToli/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TiliToli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int[] allas = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+        int[] kesz = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Button ezGomb = sender as Button;
+            Button nullaGomb = (Button)FindName("Button0");
+
+            var fTav = Math.Abs(ezGomb.Margin.Top - nullaGomb.Margin.Top);
+            var vTav = Math.Abs(ezGomb.Margin.Left - nullaGomb.Margin.Left);
+
+
+            int ezGombFelirat = int.Parse(ezGomb.Content.ToString());
+            int ezGombIndex = Array.IndexOf(allas, ezGombFelirat);
+            int nullaGombIndex = Array.IndexOf(allas, 0);
+
+            if ((fTav == 100 && vTav == 0) || (vTav == 100 && fTav == 0))
+            {
+                var seged = ezGomb.Margin;
+                ezGomb.Margin = nullaGomb.Margin;
+                nullaGomb.Margin = seged;
+
+                allas[nullaGombIndex] = allas[ezGombFelirat];
+                allas[ezGombFelirat] = 0;
+
+
+                if (allas.SequenceEqual(kesz))
+                {
+
+                }
+
+            }
+        }
+    }
+}
